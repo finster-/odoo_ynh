4e8c709cf4b2ab63996d6a3165e938c0df524b72
ljf <ljf+git@grimaud.me>
2018-12-14 03:36:58 +0100

    [enh] Install from sources

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -2,153 +2,49 @@
 # Common variables
 #
 
-APPNAME="odoo"
-
-function define_paths() {
-    command -v bc >/dev/null 2>&1 || ynh_package_install bc
-    # In odoo 10 some file change
-    if [ $(echo "$odoo_version >= 10" | bc) -ne 0 ]; then
-        if [ $(echo "$odoo_version >= 11" | bc) -ne 0 ]; then
-            export source_path=/usr/lib/python3/dist-packages/odoo/
-        else
-            export source_path=/usr/lib/python2.7/dist-packages/odoo/
-        fi
-        export conf_file=/etc/odoo/odoo.conf
-        export bin_file=/usr/bin/odoo
-    else
-        export source_path=/usr/lib/python2.7/dist-packages/openerp/
-        export conf_file=/etc/odoo/openerp-server.conf
-        export bin_file=/usr/bin/openerp-server
-    fi
-}
-
-function check_odoo_version () {
-    command -v bc >/dev/null 2>&1 || ynh_package_install bc
-    if [ $(echo "$odoo_version >= 10" | bc) -ne 0 ]; then
-        if [ -f /usr/bin/openerp-server ]; then
-            ynh_die "Another version of odoo is installed"
-        fi
-    else
-        if [ -f /usr/bin/odoo ]; then
-            ynh_die "Another version of odoo is installed"
-        fi
-    fi
-}
-
-function define_is_master() {
-    if [ -f $bin_file ]; then
-        export is_master=false
-    else
-        export is_master=true
-    fi
-}
-
-function define_port () {
-    if [ "$is_master" = true ]; then
-        export port=$(ynh_find_port 8069)
-        yunohost app checkport $port
-        if [[ ! $? -eq 0 ]]; then
-        ynh_die "Port 8069 unavailable" 1
-        fi
-    else
-        # FIXME find master port
-        export port="8069"
-    fi
-}
-
-function define_dbpass () {
-    # TODO set -x
-    if [ "$is_master" = true ]; then
-        # Generate random password
-        if [ "${1:-}" = "restore" ]; then
-            export dbpass=$(ynh_app_setting_get $app psqlpwd)
-        else
-            export dbpass=$(ynh_string_random)
-        fi
-    else
-        export dbpass=$(grep db_password /etc/odoo/odoo.conf | cut -d \= -f 2 | sed -e 's/^[ \t]*//')
-    fi
-    ynh_app_setting_set "$app" psqlpwd "$dbpass"
-}
+export APPNAME="odoo"
+DEPENDENCIES="curl postgresql xfonts-75dpi xfonts-base wkhtmltopdf node-less python3-dev gcc libldap2-dev libssl-dev libsasl2-dev python3-pip python3-dev python3-venv python3-wheel libxslt-dev libzip-dev python3-setuptools python-virtualenv python-wheel python-setuptools libjpeg-dev zlib1g-dev"
 
 # Install dependencies
 function install_dependencies() {
     ynh_add_swap 1024
-    if [ ! -f /etc/apt/sources.list.d/odoo.list ]; then
-        # Install Odoo
-        # Prepare installation
-        # We nee to setup postgresql before to let the odoo package make some magic
-        # see red comment on https://nightly.odoo.com/
-        ynh_package_install curl bc postgresql
-
-        # Install Odoo
-        curl -sS https://nightly.odoo.com/odoo.key | sudo apt-key add -
-        sh -c "echo 'deb http://nightly.odoo.com/${odoo_version}/nightly/deb/ ./' > /etc/apt/sources.list.d/odoo.list"
-        # TODO if 8.0 install https://www.odoo.com/apps/modules/8.0/shell/
-    fi
-
-    if is_jessie ; then
-        sudo echo "deb http://http.debian.net/debian jessie-backports main" | sudo tee /etc/apt/sources.list.d/jessie-backport.list
-        apt-get update
-        ynh_install_app_dependencies curl postgresql odoo xfonts-75dpi xfonts-base wkhtmltopdf node-less python-xlrd python3-dev gcc libldap2-dev libssl-dev libsasl2-dev python3-pip
-        pip3 install pyldap
-    fi
-    if is_stretch ; then
-        sudo echo "deb http://http.debian.net/debian stretch-backports main" | sudo tee /etc/apt/sources.list.d/stretch-backport.list
-        if [ $(echo "$odoo_version <= 10" | bc) -ne 0 ]; then
-            cat > /tmp/python-pypdf_1.13_all.control << EOF	# Make a control file for equivs-build
-Section: python
-Package: python-pypdf
-Version: 1.13
-Description: fake package to provide python-pypdf
-EOF
-            ynh_package_install python3-pip
-            ynh_package_install_from_equivs /tmp/python-pypdf_1.13_all.control
-            rm /tmp/python-pypdf_1.13_all.control
-            pip install pyPdf
-        fi
-        apt update
-        ynh_install_app_dependencies curl postgresql odoo xfonts-75dpi xfonts-base wkhtmltopdf node-less python-xlrd python3-dev gcc libldap2-dev libssl-dev libsasl2-dev python3-pip python3-num2words python3-pyldap python3-phonenumbers
-    fi
+    ynh_install_app_dependencies $DEPENDENCIES
 
     if ! wkhtmltopdf --version | grep "wkhtmltopdf 0.12.4 (with patched qt)"; then
         # The debian package has a bug so we deploy a more recent version
         ynh_setup_source /usr/
     fi
-}
-
-
-# Create db
-function create_general_db() {
-    service postgresql reload
-    if ! su -c "psql -lqt | cut -d \| -f 1 " - postgres | grep $APPNAME; then
-        # Generate random password
-        ynh_psql_execute_as_root "ALTER USER $APPNAME WITH CREATEDB;"
-        ynh_psql_execute_as_root "ALTER USER $APPNAME WITH PASSWORD '$dbpass';"
-        su -c "createdb -O $APPNAME $APPNAME" -  postgres
+    pushd $final_path
+    if grep "python3" $final_path/$APPNAME/$APPNAME-bin ; then
+        python3 -m venv venv
+        venv/bin/pip3 install wheel
+        venv/bin/pip3 install -r $APPNAME/requirements.txt
+    else
+        virtualenv venv
+        venv/bin/pip install wheel
+        venv/bin/pip install -r $APPNAME/requirements.txt
     fi
+    popd
 }
+
 
 # Add services
 function add_services() {
     if ! grep "^postgresql:$" /etc/yunohost/services.yml; then
         yunohost service add postgresql
     fi
-    if ! grep "^odoo:$" /etc/yunohost/services.yml; then
-        ynh_configure odoo.service /etc/systemd/system/odoo.service
-        rm /etc/init.d/odoo
-        systemctl daemon-reload
+    ynh_configure app.service /etc/systemd/system/$app.service
+    systemctl daemon-reload
 
-        yunohost service add odoo --log /var/log/odoo/odoo-server.log
-        yunohost service stop odoo
-        yunohost service start odoo
-        yunohost service enable odoo
-    fi
+    yunohost service add $app --log /var/log/$app.log
+    yunohost service stop $app
+    yunohost service start $app
+    yunohost service enable $app
 }
 
 function ssowat_and_restart() {
     # Restart odoo service
-    service odoo restart
+    service $app restart
 
     # Configure SSOWat
     ynh_sso_access "/web/database/manager"
--- scripts/_future.sh
+++ scripts/_future.sh
@@ -1,51 +1,5 @@
-
-log() {
-  echo "${1}"
-}
-
-info() {
-  log "[INFO] ${1}"
-}
-
-warn() {
-  log "[WARN] ${1}"
-}
-
-err() {
-  log "[ERR] ${1}"
-}
 ynh_check_var () {
     test -n "$1" || ynh_die "$2"
-}
-
-ynh_exit_properly () {
-    exit_code=$?
-    if [ "$exit_code" -eq 0 ]; then
-            exit 0
-    fi
-    trap '' EXIT
-    set +eu
-    echo -e "\e[91m \e[1m"
-    err "$app script has encountered an error."
-
-    if type -t CLEAN_SETUP > /dev/null; then
-        CLEAN_SETUP
-    fi
-
-    ynh_die
-}
-
-# Activate signal capture
-# Exit if a command fail, and if a variable is used unset.
-# Capturing exit signals on shell script
-#
-# example: CLEAN_SETUP () {
-#             # Clean residual file un remove by remove script
-#          }
-#          ynh_trap_on
-ynh_trap_on () {
-    set -eu
-    trap ynh_exit_properly EXIT # Capturing exit signals on shell script
 }
 
 ynh_export () {
@@ -78,42 +32,12 @@ ynh_sso_access () {
 ynh_configure () {
     local TEMPLATE=$1
     local DEST=$2
-    type j2 2>/dev/null || sudo pip install j2cli
-    j2 "${YNH_CWD}/../conf/$TEMPLATE.j2" > "${YNH_CWD}/../conf/$TEMPLATE"
-    sudo cp "${YNH_CWD}/../conf/$TEMPLATE" "$DEST"
+    ynh_render_template "${YNH_CWD}/../conf/$TEMPLATE.j2" "$DEST"
 }
 
 ynh_configure_nginx () {
     ynh_configure nginx.conf /etc/nginx/conf.d/$domain.d/$app.conf
     sudo service nginx reload
-}
-# Find a free port and return it
-#
-# example: port=$(ynh_find_port 8080)
-#
-# usage: ynh_find_port begin_port
-# | arg: begin_port - port to start to search
-ynh_find_port () {
-    port=$1
-    test -n "$port" || ynh_die "The argument of ynh_find_port must be a valid port."
-    while netcat -z 127.0.0.1 $port       # Check if the port is free
-    do
-        port=$((port+1))    # Else, pass to next port
-    done
-    echo $port
-}
-
-ynh_rm_nginx_conf () {
-    if [ -e "/etc/nginx/conf.d/$domain.d/$app.conf" ]; then
-        sudo rm "/etc/nginx/conf.d/$domain.d/$app.conf"
-        sudo service nginx reload
-    fi
-}
-
-
-ynh_secure_rm () {
-    [[ "/var/www /opt /home/yunohost.app" =~ $1 ]] \
-        || (test -n "$1" && sudo rm -Rf $1 )
 }
 
 # Upgrade
@@ -136,80 +60,8 @@ ynh_exit_if_up_to_date () {
 }
 
 
-# # Execute a command as root user
-#
-# usage: ynh_psql_execute_as_root sql [db]
-# | arg: sql - the SQL command to execute
-# | arg: db - the database to connect to
-ynh_psql_execute_as_root () {
-        sudo su -c "psql" - postgres <<< ${1}
-}
-
-# Create a user
-#
-# usage: ynh_psql_create_user user pwd [host]
-# | arg: user - the user name to create
-# | arg: pwd - the password to identify user by
-ynh_psql_create_user() {
-        ynh_psql_execute_as_root \
-        "CREATE USER ${1} WITH PASSWORD '${2}';"
-}
-
-# Create a database and grant optionnaly privilegies to a user
-#
-# usage: ynh_psql_create_db db [user [pwd]]
-# | arg: db - the database name to create
-# | arg: user - the user to grant privilegies
-# | arg: pwd - the password to identify user by
-ynh_psql_create_db() {
-    db=$1
-    # grant all privilegies to user
-    if [[ $# -gt 1 ]]; then
-        ynh_psql_create_user ${2} "${3}"
-        sudo su -c "createdb -O ${2} $db" -  postgres
-    else
-        sudo su -c "createdb $db" -  postgres
-    fi
-
-}
-
-# Drop a database
-#
-# usage: ynh_psql_drop_db db
-# | arg: db - the database name to drop
-ynh_psql_drop_db() {
-    sudo su -c "dropdb ${1}" -  postgres
-}
-
-# Drop a user
-#
-# usage: ynh_psql_drop_user user
-# | arg: user - the user name to drop
-ynh_psql_drop_user() {
-    sudo su -c "dropuser ${1}" - postgres
-}
-
-
-# Execute a command as another user
-# usage: exec_as USER COMMAND [ARG ...]
-exec_as() {
-  local USER=$1
-  shift 1
-
-  if [[ $USER = $(whoami) ]]; then
-    eval "$@"
-  else
-    # use sudo twice to be root and be allowed to use another user
-    sudo sudo -u "$USER" "$@"
-  fi
-}
-
-ynh_debian_release () {
-	lsb_release --codename --short
-}
-
 is_stretch () {
-	if [ "$(ynh_debian_release)" == "stretch" ]
+	if [ "$(ynh_get_debian_release)" == "stretch" ]
 	then
 		return 0
 	else
@@ -218,7 +70,7 @@ is_stretch () {
 }
 
 is_jessie () {
-	if [ "$(ynh_debian_release)" == "jessie" ]
+	if [ "$(ynh_get_debian_release)" == "jessie" ]
 	then
 		return 0
 	else
